bc9b5661c21517c8c28a0544c31b46b23f3490fa
sangwook16074 <qazws78941@gmail.com>
2025-10-03 15:39:09 +0900

    Initial Commit

---
+++ TodoAppExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoAppExample
+//
+//  Created by 한상욱 on 10/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoAppExample/TodoAppExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppExampleApp.swift
+//  TodoAppExample
+//
+//  Created by 한상욱 on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
